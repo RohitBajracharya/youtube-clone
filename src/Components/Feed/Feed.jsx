d9b3b2c62c9b5fd8f0d6e3945026d38e3c261d70
RohitBajra <104704874+rohitnewgit@users.noreply.github.com>
2024-04-10 12:29:47 +0545

    added feed section

---
+++ src/Components/Feed/Feed.jsx
@@ -0,0 +1,147 @@
+import React from "react";
+import thumbail1 from "../../assets/thumbnail1.png";
+import thumbail2 from "../../assets/thumbnail2.png";
+import thumbail3 from "../../assets/thumbnail3.png";
+import thumbail4 from "../../assets/thumbnail4.png";
+import thumbail5 from "../../assets/thumbnail5.png";
+import thumbail6 from "../../assets/thumbnail6.png";
+import thumbail7 from "../../assets/thumbnail7.png";
+import thumbail8 from "../../assets/thumbnail8.png";
+import "./Feed.css";
+
+function Feed() {
+  return (
+    <div className="feed">
+      <div className="card">
+        <img src={thumbail1} alt="" />
+        <h2>
+          Best channel to learn coding that help you to be a web developer
+        </h2>
+        <h3>Tour Guy</h3>
+        <p>15k views &bull; 2days ago</p>
+      </div>
+      <div className="card">
+        <img src={thumbail2} alt="" />
+        <h2>
+          Best channel to learn coding that help you to be a web developer
+        </h2>
+        <h3>Tour Guy</h3>
+        <p>15k views &bull; 2days ago</p>
+      </div>
+      <div className="card">
+        <img src={thumbail3} alt="" />
+        <h2>
+          Best channel to learn coding that help you to be a web developer
+        </h2>
+        <h3>Tour Guy</h3>
+        <p>15k views &bull; 2days ago</p>
+      </div>
+      <div className="card">
+        <img src={thumbail4} alt="" />
+        <h2>
+          Best channel to learn coding that help you to be a web developer
+        </h2>
+        <h3>Tour Guy</h3>
+        <p>15k views &bull; 2days ago</p>
+      </div>
+      <div className="card">
+        <img src={thumbail5} alt="" />
+        <h2>
+          Best channel to learn coding that help you to be a web developer
+        </h2>
+        <h3>Tour Guy</h3>
+        <p>15k views &bull; 2days ago</p>
+      </div>
+      <div className="card">
+        <img src={thumbail6} alt="" />
+        <h2>
+          Best channel to learn coding that help you to be a web developer
+        </h2>
+        <h3>Tour Guy</h3>
+        <p>15k views &bull; 2days ago</p>
+      </div>
+      <div className="card">
+        <img src={thumbail7} alt="" />
+        <h2>
+          Best channel to learn coding that help you to be a web developer
+        </h2>
+        <h3>Tour Guy</h3>
+        <p>15k views &bull; 2days ago</p>
+      </div>
+      <div className="card">
+        <img src={thumbail8} alt="" />
+        <h2>
+          Best channel to learn coding that help you to be a web developer
+        </h2>
+        <h3>Tour Guy</h3>
+        <p>15k views &bull; 2days ago</p>
+      </div>
+      <div className="card">
+        <img src={thumbail1} alt="" />
+        <h2>
+          Best channel to learn coding that help you to be a web developer
+        </h2>
+        <h3>Tour Guy</h3>
+        <p>15k views &bull; 2days ago</p>
+      </div>
+      <div className="card">
+        <img src={thumbail2} alt="" />
+        <h2>
+          Best channel to learn coding that help you to be a web developer
+        </h2>
+        <h3>Tour Guy</h3>
+        <p>15k views &bull; 2days ago</p>
+      </div>
+      <div className="card">
+        <img src={thumbail3} alt="" />
+        <h2>
+          Best channel to learn coding that help you to be a web developer
+        </h2>
+        <h3>Tour Guy</h3>
+        <p>15k views &bull; 2days ago</p>
+      </div>
+      <div className="card">
+        <img src={thumbail4} alt="" />
+        <h2>
+          Best channel to learn coding that help you to be a web developer
+        </h2>
+        <h3>Tour Guy</h3>
+        <p>15k views &bull; 2days ago</p>
+      </div>
+      <div className="card">
+        <img src={thumbail5} alt="" />
+        <h2>
+          Best channel to learn coding that help you to be a web developer
+        </h2>
+        <h3>Tour Guy</h3>
+        <p>15k views &bull; 2days ago</p>
+      </div>
+      <div className="card">
+        <img src={thumbail6} alt="" />
+        <h2>
+          Best channel to learn coding that help you to be a web developer
+        </h2>
+        <h3>Tour Guy</h3>
+        <p>15k views &bull; 2days ago</p>
+      </div>
+      <div className="card">
+        <img src={thumbail7} alt="" />
+        <h2>
+          Best channel to learn coding that help you to be a web developer
+        </h2>
+        <h3>Tour Guy</h3>
+        <p>15k views &bull; 2days ago</p>
+      </div>
+      <div className="card">
+        <img src={thumbail8} alt="" />
+        <h2>
+          Best channel to learn coding that help you to be a web developer
+        </h2>
+        <h3>Tour Guy</h3>
+        <p>15k views &bull; 2days ago</p>
+      </div>
+    </div>
+  );
+}
+
+export default Feed;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import Feed from "../../Components/Feed/Feed";
 import Sidebar from "../../Components/Sidebar/Sidebar";
+import "./Home.css";
 
 const Home = ({ sidebar }) => {
   return (
     <>
       <Sidebar sidebar={sidebar} />
+      <div className={`container ${sidebar ? "" : "large-container"}`}>
+        <Feed />
+      </div>
     </>
   );
 };
