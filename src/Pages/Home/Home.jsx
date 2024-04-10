e216b01edfbb25c99685fef2e32a4c9ee37152c1
RohitBajra <104704874+rohitnewgit@users.noreply.github.com>
2024-04-10 12:03:34 +0545

    added navbar and sidebar

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Sidebar from "../../Components/Sidebar/Sidebar";
+
+const Home = ({ sidebar }) => {
+  return (
+    <>
+      <Sidebar sidebar={sidebar} />
+    </>
+  );
+};
+
+export default Home;
